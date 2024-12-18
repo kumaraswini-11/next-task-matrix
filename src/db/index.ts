25008253c7e73be25755c46c4f26bd2b1eaa290a
kumaraswini-11 <kumaraswini.11.11@gmail.com>
2024-12-18 09:30:49 +0530

    Drizzel ORM Setup corrections

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,14 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-serverless";
+import { config } from "dotenv";
+
+config({ path: ".env" });
 
 const sql = neon(process.env.DATABASE_URL!);
-const db = drizzle({ client: sql as any });
+// @ts-ignore
+const db = drizzle({ client: sql as any, casing: "snake_case" });
 
 // Logger
-// const db = drizzle(sql as any, { logger: true });
+// const db = drizzle(sql as any, { casing: "snake_case", logger: true });
 
 export { db };
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,44 +1,48 @@
-import {
-  pgTable,
-  serial,
-  varchar,
-  boolean,
-  timestamp,
-  check,
-  index,
-  uniqueIndex,
-} from "drizzle-orm/pg-core";
-import { relations, sql } from "drizzle-orm";
+// import { AnyPgColumn } from "drizzle-orm/pg-core";
+import { pgEnum, pgTable as table } from "drizzle-orm/pg-core";
+import * as t from "drizzle-orm/pg-core";
+// import { relations } from "drizzle-orm";
 
-export const usersTable = pgTable(
+const timestamps = {
+  created_at: t.timestamp().defaultNow().notNull(),
+  updated_at: t.timestamp(),
+  // deleted_at: t.timestamp(),
+};
+
+export const rolesEnum = pgEnum("roles_enum", [
+  "admin",
+  "member",
+  "manager",
+  "client",
+]);
+
+export const authProviderEnum = pgEnum("auth_providers_enum", [
+  "google",
+  "github",
+  "credential",
+]);
+
+export const usersTable = table(
   "users",
   {
-    id: serial("id").primaryKey(),
-    name: varchar({ length: 255 }).notNull(),
-    email: varchar({ length: 255 }).notNull().unique(),
-    phone: varchar("phone", { length: 255 }).unique(),
-    role: varchar({ length: 255 }).notNull(),
-    password: varchar({ length: 255 }).notNull(),
-    authProvider: varchar("auth_provider", { length: 255 }).notNull(),
-    createdAt: timestamp("created_at").notNull().defaultNow(),
-    updatedAt: timestamp("updated_at")
-      .notNull()
-      .defaultNow()
-      .$onUpdate(() => new Date()),
-    isActive: boolean("is_active").notNull().default(true),
-    avatarUrl: varchar("avatar_url", { length: 500 }),
+    id: t.serial().primaryKey(),
+    name: t.varchar({ length: 255 }).notNull(),
+    email: t.varchar({ length: 255 }).notNull().unique(),
+    phone: t.varchar({ length: 255 }).unique(),
+    avatarUrl: t.varchar("avatar_url", { length: 500 }),
+    password: t.varchar({ length: 255 }).notNull(),
+    authProvider: authProviderEnum("auth_provider").default("credential"),
+    role: rolesEnum().default("member"),
+    isActive: t.boolean("is_active").default(true),
+    ...timestamps,
   },
   (table) => {
     return [
       {
-        activeIdx: index("is_active_idx").on(table.isActive),
-        emailIdx: uniqueIndex("email_idx").on(table.email),
-        phoneIdx: uniqueIndex("phone_idx").on(table.phone),
+        emailIndex: t.uniqueIndex("email_idx").on(table.email),
+        phoneIndex: t.uniqueIndex("phone_idx").on(table.phone),
+        isActiveIndex: t.index("is_active_idx").on(table.isActive),
       },
-      check(
-        "auth_provider_check",
-        sql`${table.authProvider} IN ('google', 'github', 'credential')`,
-      ),
     ];
   },
 );
